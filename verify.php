a23e8076023aa28a32c00ce6a7bf163b932289b3
ibiixer <ibiixer@gmail.com>
2017-06-15 11:50:10 +0700

    Create verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'ssSrOykgfMcmaYm5M/Q2N9meK8+eyQlGODJbDK+tOkMjD3vSZRXL1XOIJvMo8IKrwmnVGsBwkCkm/Y/moWYc8mpRzlgozz9RbDwHyg5/iJsolFd8HDxxKD72UgDlFPnaNOHLk/MMV+eShIVnxB8JMAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . ssSrOykgfMcmaYm5M/Q2N9meK8+eyQlGODJbDK+tOkMjD3vSZRXL1XOIJvMo8IKrwmnVGsBwkCkm/Y/moWYc8mpRzlgozz9RbDwHyg5/iJsolFd8HDxxKD72UgDlFPnaNOHLk/MMV+eShIVnxB8JMAdB04t89/1O/w1cDnyilFU=);
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
